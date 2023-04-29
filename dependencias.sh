1545da0c895c60ff6a244e3c85c110934b58d5a9
Ítalo Leite da Silva <italoleitecg@gmail.com>
2023-04-29 19:39:19 -0300

    deployment add kubernetes

--- dependencias.sh
+++ dependencias.sh
@@ -5,6 +5,7 @@ sudo apt-get install -y apt-utils
 sudo apt-get install -y curl
 sudo apt-get install -y wget
 sudo apt-get install -y git
+sudo apt install vim -y
 
 sudo apt-get install -y ca-certificates curl gnupg lsb-release
 
@@ -23,19 +24,32 @@ sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # installing the kubernetes
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl
-sudo apt-get install -y apt-transport-https
-sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
+## sudo apt-get update
+## sudo apt-get install -y ca-certificates curl
+## sudo apt-get install -y apt-transport-https
+## sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+## echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+## sudo apt-get update
+## sudo apt-get install -y kubectl
+
+#installing the k3d
+wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+#installing kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
 # docker run --name mariadb -e MARIADB_ROOT_PASSWORD=diouxx -e MARIADB_DATABASE=glpidb -e MARIADB_USER=glpi_user -e MARIADB_PASSWORD=glpi -d mariadb:10.7
 # docker run --name glpi --link mariadb:mariadb -p 80:80 -d diouxx/glpi
+# docker run -d --name=grafana-italo -p 3000:3000 italoleitecg/grafana-italo
 
-docker run -d --name=grafana-italo -p 3000:3000 italoleitecg/grafana-italo
+
 
 sudo su -
 cd /root
-wget https://raw.githubusercontent.com/italoleitecg/iib-technical-challenge/main/deployment.yaml
+wget https://github.com/italoleitecg/technical-challenge/blob/main/deployment.yaml
+
+
+
+
+k3d cluster create meucluster -p "80:30000@loadbalancer"
